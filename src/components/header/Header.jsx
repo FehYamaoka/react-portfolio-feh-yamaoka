68b4447d832b449b7105008f453252b5dea7c509
FehYamaoka <fehyamaoka@gmail.com>
2022-09-13 04:53:17 -0300

    feat: Header Component JSX | React Icons | Header Component CSS - Responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 import "./header.css";
+import { CTA } from './CTA';
+import ME from '../../assets/me.png';
+import { HeaderSocials } from "./HeaderSocials";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Olá, Eu sou o</h5>
+        <h1>Felipe Yamaoka</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+
+        <a href="#contact" className="scroll__down">Role para baixo</a>
+      </div>
+    </header>
+  )
 };
---
+++ src/components/header/Lamp/Lamp.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import './lamp.css';
+
+export const Lamp = () => {
+  return (
+    <div>
+      <div className="lamp">
+        <div className="top-cover"></div>
+
+        <div className="bubble1"></div>
+        <div className="bubble2"></div>
+        <div className="bubble3"></div>
+
+        <div className="blob1"></div>
+        <div className="blob2"></div>
+        <div className="blob3"></div>
+
+        <div className="bottom-cover"></div>
+      </div>
+    </div>
+  )
+}
